58a538ef3c5af52bb95d8d441c967be0b087154d
Wess L. Toft <weslyn.l.toft@gmail.com>
2025-10-07 11:04:33 +0200

    Task 6 complete

--- js/index.js
+++ js/index.js
@@ -10,38 +10,68 @@ function findNode(id, type) {
     node.style.backgroundColor = "lightgray";
 
 
-    let newNode = document.createElement(type)
-    newNode.innerText = "I am a new node";
-    newNode.style.color = "yellow"; 
-    node.appendChild(newNode);
    
     return node;
 }
 
-function deleteNode(id, type) {
+function addNode(item, type) {
+    let node = document.getElementById("list");
+    let newNode = document.createElement(type);
+    newNode.innerText = item;
+    newNode.style.color = "yellow"; 
+    node.appendChild(newNode);
+
+    return node; 
+}
+
+function deleteNode(id) {
     let node = document.getElementById(id);
     node.remove();
+
     return node;
 }
 
-function editNode(id, type) {
+function editNode(id) {
     let node = document.getElementById(id);
     node.textContent = "I am an edited node";
+
     return node;
+}
+
+function listCounter() {
+    let listItems = document.querySelectorAll("li");
+    console.log("Number of list items: " + listItems.length);
+
+    return listItems.length;
 }
 
 
 
 
-
+//find node
 let newNode = findNode("list", "li");
-console.log("New node is: ");
+console.log("Found node is: ");
 console.log(newNode);
 
+//add node
+let addedNode = addNode("Peanuts", "list");
+console.log("Added node is: ");
+console.log(addedNode);
+
+let addedNodeTwo = addNode("Bacon", "list");
+console.log("Added node is: ");
+console.log(addedNodeTwo);
+
+//delete node
 let deletedNode = deleteNode("0", "li");
 console.log("Deleted node is: ");
 console.log(deletedNode);
 
+//edit node
 let editedNode = editNode("1", "li");
 console.log("Edited node is: ");
 console.log(editedNode);
+
+//list counter
+let listCount = listCounter();
+console.log("List count is: " + listCount);
