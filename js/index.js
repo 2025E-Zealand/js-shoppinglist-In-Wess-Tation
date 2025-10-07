49823f0e988ccb467bdbee043ad3db9a1cc8a559
Wess L. Toft <weslyn.l.toft@gmail.com>
2025-10-07 10:47:58 +0200

    Task 4 complete

--- js/index.js
+++ js/index.js
@@ -1,2 +1,38 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function findNode(id, type) {
+    let node = document.getElementById(id);
+    node.style.color = "green";
+    node.style.fontSize = "24px";
+    node.style.fontWeight = "bold";
+    node.style.textDecoration = "underline";
+    node.style.backgroundColor = "lightgray";
+
+
+    let newNode = document.createElement(type)
+    newNode.innerText = "I am a new node";
+    newNode.style.color = "yellow"; 
+    node.appendChild(newNode);
+   
+    return node;
+}
+
+function deleteNode(id, type) {
+    let node = document.getElementById(id);
+    node.remove();
+    return node;
+}
+
+
+
+
+
+
+let newNode = findNode("list", "li");
+console.log("New node is: ");
+console.log(newNode);
+
+let deletedNode = deleteNode("0", "li");
+console.log("Deleted node is: ");
+console.log(deletedNode);
